46e2ffa62f209b04fa6f2c0a68dc211fc4c125e5
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-22 06:04:22 -0400

    feat: expose maTumbo workspace through package and CI

--- src/core/index.js
+++ src/core/index.js
@@ -2,6 +2,7 @@ export {createRealityLattice, RealityLatticeEngine} from './reality-engine.js';
 export {createRealityState, cloneRealityState, normalizeEntity, recalculateSummaries, recordHistory} from './reality-state.js';
 export {MODEL_CATEGORIES, STANDARD, QUANTUM_BRANCHING, CPT_MIRROR, ANTIMATTER, ACTIVE_RESEARCH, HIGHER_DIMENSION, CUSTOM_LAWS, RULESETS, getRuleSet} from './physics-rules.js';
 export {invertMatterState, mirrorState, transformState, rewindState} from './transformations.js';
-
 export {addCausalEvent, canReach, causalSummary} from './causality.js';
 export {stepReality, simulationInfo} from './physics-simulation.js';
+export {createOutcomeContracts, OUTCOME_CONTRACT_SCHEMA, SIMULATION_UNIT, STATUSES} from './outcome-contracts.js';
+export {createMatumboReality, MATUMBO_REALITY_SCHEMA, SURFACES, ROOMS, WARDROBE, BLOCKS} from './matumbo-reality.js';
