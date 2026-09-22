6dc41c916602a405a7bf0e5f83d8b93e2dec6fe5
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-22 05:20:26 -0400

    feat: export simulation and causality primitives

--- src/core/index.js
+++ src/core/index.js
@@ -2,3 +2,6 @@ export {createRealityLattice, RealityLatticeEngine} from './reality-engine.js';
 export {createRealityState, cloneRealityState, normalizeEntity, recalculateSummaries, recordHistory} from './reality-state.js';
 export {MODEL_CATEGORIES, STANDARD, QUANTUM_BRANCHING, CPT_MIRROR, ANTIMATTER, ACTIVE_RESEARCH, HIGHER_DIMENSION, CUSTOM_LAWS, RULESETS, getRuleSet} from './physics-rules.js';
 export {invertMatterState, mirrorState, transformState, rewindState} from './transformations.js';
+
+export {addCausalEvent, canReach, causalSummary} from './causality.js';
+export {stepReality, simulationInfo} from './physics-simulation.js';
