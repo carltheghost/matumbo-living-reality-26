b2465beb413ac51e040bad9b2e1dff70289e8b91
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-22 05:12:26 -0400

    feat: add public core exports

---
+++ src/core/index.js
@@ -0,0 +1,4 @@
+export {createRealityLattice, RealityLatticeEngine} from './reality-engine.js';
+export {createRealityState, cloneRealityState, normalizeEntity, recalculateSummaries, recordHistory} from './reality-state.js';
+export {MODEL_CATEGORIES, STANDARD, QUANTUM_BRANCHING, CPT_MIRROR, ANTIMATTER, ACTIVE_RESEARCH, HIGHER_DIMENSION, CUSTOM_LAWS, RULESETS, getRuleSet} from './physics-rules.js';
+export {invertMatterState, mirrorState, transformState, rewindState} from './transformations.js';
